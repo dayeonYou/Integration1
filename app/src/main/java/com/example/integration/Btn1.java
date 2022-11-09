c9121a32bee46d41f2a2e8613189759f5064226d
dayeonYou <0620dadada@suwon.ac.kr>
2022-11-10 00:42:26 +0900

    integration1

--- app/src/main/java/com/example/integration/Btn1.java
+++ app/src/main/java/com/example/integration/Btn1.java
@@ -2,48 +2,57 @@ package com.example.integration;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Btn1 extends AppCompatActivity {
-    String videoUrl = "https://media.geeksforgeeks.org/wp-content/uploads/20201217192146/Screenrecorder-2020-12-17-19-17-36-828.mp4?_=1";
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+public class Btn1 extends AppCompatActivity {
+    String videoUrl = "abababa";
+    static FirebaseDatabase database;
+    static DatabaseReference databaseReference;
     @Override
     protected void onCreate(@Nullable Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.btn1);
         setTitle("CAMERA");
-
-        // finding videoview by its id
-        VideoView videoView = findViewById(R.id.videoView);
-
-        // Uri object to refer the
-        // resource from the videoUrl
-        Uri uri = Uri.parse(videoUrl);
-
-        // sets the resource from the
-        // videoUrl to the videoView
-        videoView.setVideoURI(uri);
-
-        // creating object of
-        // media controller class
         MediaController mediaController = new MediaController(this);
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Video/url");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                videoUrl = dataSnapshot.getValue().toString();
+                VideoView videoView = findViewById(R.id.videoView);
 
-        // sets the anchor view
-        // anchor view for the videoView
-        mediaController.setAnchorView(videoView);
+                Uri uri = Uri.parse(videoUrl);
 
-        // sets the media player to the videoView
-        mediaController.setMediaPlayer(videoView);
+                videoView.setVideoURI(uri);
 
-        // sets the media controller to the videoView
-        videoView.setMediaController(mediaController);
+                mediaController.setAnchorView(videoView);
 
-        // starts the video
-        videoView.start();
+                mediaController.setMediaPlayer(videoView);
 
+                videoView.setMediaController(mediaController);
+
+                videoView.start();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("MainActivity", String.valueOf(databaseError.toException()));
+            }
+        });
     }
 }
