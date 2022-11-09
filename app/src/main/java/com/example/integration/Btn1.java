d7c3cad42abc7824737ecf08245748c2f5690c05
dayeonYou <0620dadada@suwon.ac.kr>
2022-11-09 23:05:32 +0900

    integration1

--- app/src/main/java/com/example/integration/Btn1.java
+++ app/src/main/java/com/example/integration/Btn1.java
@@ -1,16 +1,49 @@
 package com.example.integration;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Btn1 extends AppCompatActivity {
+    String videoUrl = "https://media.geeksforgeeks.org/wp-content/uploads/20201217192146/Screenrecorder-2020-12-17-19-17-36-828.mp4?_=1";
 
     @Override
     protected void onCreate(@Nullable Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.btn1);
         setTitle("CAMERA");
+
+        // finding videoview by its id
+        VideoView videoView = findViewById(R.id.videoView);
+
+        // Uri object to refer the
+        // resource from the videoUrl
+        Uri uri = Uri.parse(videoUrl);
+
+        // sets the resource from the
+        // videoUrl to the videoView
+        videoView.setVideoURI(uri);
+
+        // creating object of
+        // media controller class
+        MediaController mediaController = new MediaController(this);
+
+        // sets the anchor view
+        // anchor view for the videoView
+        mediaController.setAnchorView(videoView);
+
+        // sets the media player to the videoView
+        mediaController.setMediaPlayer(videoView);
+
+        // sets the media controller to the videoView
+        videoView.setMediaController(mediaController);
+
+        // starts the video
+        videoView.start();
+
     }
 }
